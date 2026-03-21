57c531470cadab9fbad556a0e2668df337b80962
jorry <jorry.p@gmail.com>
2026-03-21 18:53:36 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>iFortaleza 🌴</h1>
+      <p>The Ultimate Fortaleza City Guide</p>
+    </main>
+  );
+}
